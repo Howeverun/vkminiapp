93b853f9977845f5c6221a24d397297847a6a67a
Гудым Артём <sabbirbeb@mail.ru>
2020-05-06 10:40:22 +0300

    Updates

---
+++ precache-manifest.0ec7c77dc316ecb0331f9af88cdfe0fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d3e36b87a440acad6b9a7253524b87",
+    "url": "/vkminiapp/index.html"
+  },
+  {
+    "revision": "18fa28165a8681bd5ae9",
+    "url": "/vkminiapp/static/css/2.551d3f2e.chunk.css"
+  },
+  {
+    "revision": "5a2a9255d7279cd9cca2",
+    "url": "/vkminiapp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "18fa28165a8681bd5ae9",
+    "url": "/vkminiapp/static/js/2.5f73a1b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vkminiapp/static/js/2.5f73a1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2a9255d7279cd9cca2",
+    "url": "/vkminiapp/static/js/main.b654b958.chunk.js"
+  },
+  {
+    "revision": "7cab768db9fe218f6114",
+    "url": "/vkminiapp/static/js/runtime-main.946394a3.js"
+  },
+  {
+    "revision": "b833ebd4a645791f491b9e3ed12e6a65",
+    "url": "/vkminiapp/static/media/tomato.b833ebd4.PNG"
+  }
+]);

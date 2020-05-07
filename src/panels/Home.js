9ce592297c9161c3c06e893f55b1c0d54fbc8b64
Гудым Артём <sabbirbeb@mail.ru>
2020-05-07 21:03:02 +0300

    nichego ne menyau baluus'

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -26,6 +26,7 @@ const Home = ({ id, go, fetchedUser }) => (
 				<Button size="xl" level="2" onClick={go} data-to="persik">
 					Show me the Persik, please
 				</Button>
+                <p></p>
 				<Button size="xl" level="2" onClick={go} data-to="secondpersik">
 					Show me the secondPersik, please
 				</Button>

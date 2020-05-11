e52a2adb5e21e5b685b32b5fa5fb21ac016162dc
Гудым Артём <sabbirbeb@mail.ru>
2020-05-12 01:08:56 +0300

    изменил старт не знаю как жить

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import View from '@vkontakte/vkui/dist/components/View/View';
@@ -12,43 +14,69 @@ import Boryapractiсe from './panels/Boryapractiсe';
 import Dashapractiсe from './panels/Dashapractiсe';
 import Linapractiсe from './panels/Linapractiсe';
 
-const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(null);//useState(<ScreenSpinner size='large'/>);
 
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+import { Stack, Page, Tabbar } from "vkui-navigator/dist";
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+import Panel1 from "./panels/Panel1";
+import Panel2 from "./panels/Panel2";
+import Panel3 from "./panels/Panel3";
+import Panel4 from "./panels/Panel4";
+import Modal1 from "./panels/Modal1";
+import Modal2 from "./panels/Modal2";
 
-	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
-			<Secondpersik id='secondpersik' go={go} />
-			<Artempractiсe id='artempractiсe' go={go} />
-			<Boryapractiсe id='boryapractiсe' go={go} />
-			<Dashapractiсe id='dashapractiсe' go={go} />
-			<Linapractiсe id='linapractiсe' go={go} />
-		</View>
-	);
-}
+import Icon24Services from "@vkontakte/icons/dist/24/services";
+import Icon24Flash from "@vkontakte/icons/dist/24/flash";
+
+const App = () => (
+
+
+	<Stack
+		activePage="main"
+		modals={[
+			<Modal1 id="modal1"/>,
+			<Modal2 id="modal2" title="Название из title"/>
+		]}
+	>
+		<Tabbar
+			id="main"
+			activeStory="home"
+		>
+			<Page
+				id="home"
+				title="Основы"
+				activePanel="panel1"
+				icon={<Icon24Services/>}
+			>
+				<Panel1 id="panel1"/>
+				<Panel2 id="panel2"/>
+			</Page>
+			<Page
+				id="settings"
+				title="Модальные окна"
+				activePanel="panel3"
+				icon={<Icon24Flash/>}
+			>
+				<Panel3 id="panel3"/>
+			</Page>
+		</Tabbar>
+		<Page id="page2" activePanel="panel4">
+			<Panel4 id="panel4"/>
+		</Page>
+	</Stack>
+
+
+);
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
 

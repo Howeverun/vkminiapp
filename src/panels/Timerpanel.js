854e098eb9d123f185ec6e87344d903eb7426762
dasha <onikovad@gmail.com>
2020-05-16 13:50:23 +0300

    timer works

--- src/panels/Timerpanel.js
+++ src/panels/Timerpanel.js
@@ -9,17 +9,55 @@ import PropTypes from "prop-types";
 import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
 import {IOS} from "@vkontakte/vkui";
 
+function countdown(){
+	let division = document.getElementById("time")
+	let notify = document.getElementById("notify")
+	let sec = 0;
+	let min = 0;
+	let min_to = 25;
+	let next_min = 2;
+	setInterval(function() {
+		let first = ""
+		let second = ""
+		if(min<10){
+			first = "0"
+		}
+		if(sec<10){
+			second = "0"
+		}
+		division.textContent = first + min + " : " + second + sec;
+	
+		if(sec === 59){
+			sec = 0;
+			min++;
+			if (min===min_to){
+				notify.textContent = "Vse"
+				min = 0;
+				sec = 0;
+			}
+		}
+		sec++;
+
+	}, 1000)
+}
 
 const timerPanel = ({ id, navigator }) => (
 	<Panel id={id}>
 		<PanelHeader>
 		    timeeeer
 		</PanelHeader>
-		 AAAAAAA
-
 		<Group title="Параметры">
 			<Div>
 				{JSON.stringify(navigator.params)}
+			</Div>
+		</Group>
+		<Group title="Timer">
+			<Button onClick={countdown}> Начать таймер </Button>
+			<Div id={"time"}>
+
+			</Div>
+			<Div id={"notify"}>
+
 			</Div>
 		</Group>
 	</Panel>

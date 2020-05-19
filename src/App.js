7a2a30130b1e62996e1be88d7a561e4ec175032c
Гудым Артём <sabbirbeb@mail.ru>
2020-05-19 21:18:23 +0300

    merge commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import View from '@vkontakte/vkui/dist/components/View/View';
--- src/panels/Timerpanel.js
+++ src/panels/Timerpanel.js
@@ -7,7 +7,15 @@ import Group from "@vkontakte/vkui/dist/components/Group/Group";
 import Div from "@vkontakte/vkui/dist/components/Div/Div";
 import PropTypes from "prop-types";
 import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
-import {IOS} from "@vkontakte/vkui";
+import { platform, IOS } from '@vkontakte/vkui';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+
+
+const osName = platform();
+
+
+
 
 function countdown(){
 	let division = document.getElementById("time")
@@ -15,7 +23,8 @@ function countdown(){
 	let sec = 0;
 	let min = 0;
 	let min_to = 25;
-	let id = setInterval(function() {
+	let next_min = 2;
+	setInterval(function() {
 		let first = ""
 		let second = ""
 		if(min<10){
@@ -31,16 +40,8 @@ function countdown(){
 			min++;
 			if (min===min_to){
 				notify.textContent = "Vse"
-				if(min<10){
-					first = "0"
-				}
-				if(sec<10){
-					second = "0"
-				}
-				division.textContent = first + min + " : " + second + sec;
 				min = 0;
 				sec = 0;
-				clearInterval(id);
 			}
 		}
 		sec++;
@@ -48,44 +49,25 @@ function countdown(){
 	}, 1000)
 }
 
-const timerPanel = ({ id, navigator }) => (
-	<Panel id={id}>
-		<PanelHeader>
-		    timeeeer
+
+const timerPanel = props => (
+	<Panel id={props.id}>
+		<PanelHeader
+			left={<PanelHeaderButton onClick={props.go} data-to="home">
+				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+			</PanelHeaderButton>}
+		>
+			Persik
 		</PanelHeader>
-		<Group title="Параметры">
-		</Group>
-		<Group title="Timer">
-			<Button onClick={countdown}> Начать таймер </Button>
-			<Div id={"time"}>
-
-			</Div>
-			<Div id={"notify"}>
-
-			</Div>
-		</Group>
+					<Button onClick={countdown}> Начать таймер </Button>
 	</Panel>
 );
-/*
-const timerPanel = ({ id, navigator })  => (
-    <Panel id={id}>
-        <PanelHeader>
 
-         timeeeer
-        </PanelHeader>
-            AAAAAAA
-    </Panel>
-    /*ReactDOM.render(
-        <div style={{
-            border: "8px solid",
-            borderImage: "linear-gradient(-45deg, #743ad5, #d53a9d) 1"
-        }}>
-            Время:<br/>
-            {new Date().toLocaleTimeString()}
-        </div>, document.getElementById('root'));
-}, 1000); */
+timerPanel.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+};
 
-//);
 
 
 export default timerPanel;

c0c8bb0b9671b734462acecd97c3b5211baf39a6
Гудым Артём <sabbirbeb@mail.ru>
2020-07-13 19:53:11 +0300

    2

--- src/oldApp.js
+++ src/oldApp.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import View from '@vkontakte/vkui/dist/components/View/View';
@@ -7,81 +6,60 @@ import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
 import Persik from './panels/Persik';
-import SecondPersik from './panels/Secondpersik';
-import Artempractiсe2 from './panels/Artempractiсe2';
+import Secondpersik from './panels/Secondpersik';
+import Artempractiсe from './panels/Artempractiсe';
 import Boryapractiсe from './panels/Boryapractiсe';
 import Dashapractiсe from './panels/Dashapractiсe';
 import Linapractiсe from './panels/Linapractiсe';
-//##########
 import Timerpanel from './panels/Timerpanel';
-//##########
-import { Stack, Page, Tabbar } from "vkui-navigator/dist";
-
-import Panel1 from "./panels/Panel1";
-import Panel2 from "./panels/Panel2";
-import Panel3 from "./panels/Panel3";
-import Panel4 from "./panels/Panel4";
-import Modal1 from "./panels/Modal1";
-import Modal2 from "./panels/Modal2";
+import Intro from './panels/Intro';
 
 
-import Icon24Services from "@vkontakte/icons/dist/24/services";
-import Icon24Flash from "@vkontakte/icons/dist/24/flash";
-
-    bridge.send("VKWebAppInit", {});
-    bridge.send("VKWebAppAllowNotifications", {});
-
+const ROUTES = {
+	HOME: 'home',
+	INTRO: 'intro',
+};
 
 
-const App = () => (
+const App = () => {
+	const [activePanel, setActivePanel] = useState(ROUTES.INTRO); //стартовая страница
+	const [fetchedUser, setUser] = useState(null);
+	const [popout, setPopout] = useState(null);//useState(<ScreenSpinner size='large'/>);
 
+	useEffect(() => {
+		bridge.subscribe(({ detail: { type, data }}) => {
+			if (type === 'VKWebAppUpdateConfig') {
+				const schemeAttribute = document.createAttribute('scheme');
+				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+				document.body.attributes.setNamedItem(schemeAttribute);
+			}
+		});
+		async function fetchData() {
+			const user = await bridge.send('VKWebAppGetUserInfo');
+			setUser(user);
+			setPopout(null);
+		}
+		fetchData();
+	}, []);
 
-	<Stack
-		activePage="main"
-		modals={[
-			<Modal1 id="modal1"/>,
-			<Modal2 id="modal2" title="Название из title"/>
-		]}
-	>
-		<Tabbar
-			id="main"
-			activeStory="home"
-		>
-			<Page
-				id="home"
-				title="Основы"
-				activePanel="panel1"
-				icon={<Icon24Services/>}
-			>
-				<Panel1 id="panel1"/>
-				<Panel2 id="panel2"/>
-				<Timerpanel id="Timerpanel"/>
-			</Page>
+	const go = e => {
+		setActivePanel(e.currentTarget.dataset.to);
+	};
 
-			<Page
-				id="settings"
-				title="Модальные окна"
-				activePanel="panel3"
-				icon={<Icon24Flash/>}
-			>
-				<Panel3 id="panel3"/>
-			</Page>
+	return (
+		<View activePanel={activePanel} popout={popout}>
+			<Home id='home' fetchedUser={fetchedUser} go={go} />
+			<Persik id='persik' go={go} />
+			<Secondpersik id='secondpersik' go={go} />
+			<Artempractiсe id='artempractiсe' go={go} />
+			<Boryapractiсe id='boryapractiсe' go={go} />
+			<Dashapractiсe id='dashapractiсe' go={go} />
+			<Linapractiсe id='linapractiсe' go={go} />
+			<Timerpanel id='timerpanel' go={go} />
 
-			<Page
-				id="you_are"
-				title="О тебе"
-				activePanel="Artempractiсe2"
-				icon={<Icon24Services/>}
-			>
-			<Artempractiсe2 id="Artempractiсe2"/>
-			</Page>
-		</Tabbar>
-		<Page id="page2" activePanel="panel4">
-			<Panel4 id="panel4"/>
-		</Page>
-	</Stack>
-
-
-);
+			<Intro id='intro' go={go} />
+		</View>
+	);
+}
 
 export default App;
